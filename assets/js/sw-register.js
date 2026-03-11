113606cc5e354b1011d7c4969cd3a0651ac84571
andywusz88 <andywusz88@gmail.com>
2026-03-11 14:53:30 +0800

    Delete assets directory

--- assets/js/sw-register.js
+++
@@ -1,9 +0,0 @@
-// Register service worker (GitHub Pages HTTPS)
-(function(){
-  if (!('serviceWorker' in navigator)) return;
-  window.addEventListener('load', function(){
-    navigator.serviceWorker.register('./sw.js').catch(function(err){
-      console.warn('SW register failed', err);
-    });
-  });
-})();
